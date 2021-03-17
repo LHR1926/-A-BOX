237ac7afc1cd8ac8c644ce1891a0abc5337e9867
LHR1926 <51528762+LHR1926@users.noreply.github.com>
2021-03-17 23:32:54 +0800

    Add files via upload

---
+++ A broken calculator.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int ans = 0;
+        int t = Y;
+        while (t > X){
+            if ((t & 1) == 0){
+                ans += 1;
+                t = t >> 1 ;               
+            }
+            else{
+                ans += 2;
+                t += 1;
+                t = t >> 1;
+            }            
+        }
+        ans += X - t;
+        return ans;
+    }
+};
