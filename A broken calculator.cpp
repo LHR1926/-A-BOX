5a147e7e6fffbc2346e72682becb2751b966d8b2
LHR1926 <51528762+LHR1926@users.noreply.github.com>
2021-03-31 23:37:35 +0800

    Add files via upload

--- A broken calculator.cpp
+++ A broken calculator.cpp
@@ -1,20 +1,66 @@
-class Solution {
-public:
-    int brokenCalc(int X, int Y) {
-        int ans = 0;
-        int t = Y;
-        while (t > X){
-            if ((t & 1) == 0){
-                ans += 1;
-                t = t >> 1 ;               
-            }
-            else{
-                ans += 2;
-                t += 1;
-                t = t >> 1;
-            }            
+#pragma warning(disable : 4996)
+#include <stdio.h>
+#include <string.h>
+
+char plain[100], cry[100];
+int i, key;
+
+int encrypt()
+{
+    printf("请输入明文:");
+    scanf("%s", &plain[i]);
+    printf("请输入密钥(1-25):");
+    scanf("%d", &key);
+    for (i = 0; i < strlen(plain); i++)
+    {
+        if (plain[i] >= 'A' && plain[i] <= 'Z')
+        {
+            plain[i] = (plain[i] - 'A' + key) % 26 + 'A';
         }
-        ans += X - t;
-        return ans;
+        else if (plain[i] >= 'a' && plain[i] <= 'z')
+        {
+            plain[i] = (plain[i] - 'a' + key) % 26 + 'a';
+        }
     }
-};
+    printf("%s", plain);
+    printf("\n");
+    return 0;
+}
+
+int decrypt()
+{
+    printf("请输入密文:");
+    scanf("%s", &cry[i]);
+    printf("请输入密钥(1-25):");
+    scanf("%d", &key);
+    for (i = 0; i < strlen(cry); i++)
+    {
+        if (cry[i] >= 'A' && cry[i] <= 'Z')
+        {
+            cry[i] = (cry[i] - 'A' - key) % 26 + 'A';
+        }
+        else if (cry[i] >= 'a' && cry[i] <= 'z')
+        {
+            cry[i] = (cry[i] - 'a' - key) % 26 + 'a';
+        }
+    }
+    printf("%s", cry);
+    printf("\n");
+    return 0;
+}
+
+int main()
+{
+    int c;
+    printf("加密选择1，解密选择2:");
+    scanf("%d", &c);
+    if (c == 1)
+    {
+        encrypt();
+    }
+    if (c == 2)
+    {
+        decrypt();
+    }
+    return 0;
+}
